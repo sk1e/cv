86e23493b7f8f0cf92e9d0958974c1182a586ca6
Sergey Petrov <sekk1e@yandex.ru>
2016-10-31 14:58:53 +0700

    + section title mixin

--- cv.js
+++ cv.js
@@ -5,3 +5,5 @@ require('./fonts.styl');
 require('./blocks/photo.styl');
 require('./blocks/author.styl');
 require('./blocks/cv.styl');
+require('./blocks/title.styl');
+require('./icons.styl');
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ var path = require('path');
 var NpmInstallPlugin = require('npm-install-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var SpritesmithPlugin = require('webpack-spritesmith');
 
 module.exports = {
   entry: './cv.js',
@@ -17,14 +18,29 @@ module.exports = {
     loaders: [
         { test: /\.pug$/, loader: 'pug' },
         { test: /\.styl$/, loader: ExtractTextPlugin.extract('!css!stylus') },
-        { test: /\.(svg|ttf|eot|woff|woff2)$/, loader: 'file?name=[path][name].[ext]' }
+        { test: /\.(png|svg|ttf|eot|woff|woff2)$/, loader: 'file?name=[path][name].[ext]' }
     ]
   },
+    resolve: {
+        modulesDirectories: ["web_modules", "node_modules", "icons"]
+    },
     plugins: [
         new NpmInstallPlugin(),
         new HtmlWebpackPlugin({template: "index.pug"}),
         new ExtractTextPlugin('[name].css', {allChunks: true}),
-        
+        new SpritesmithPlugin({
+            src: {
+                cwd: path.resolve(__dirname, './icons'),
+                glob: '*.png'
+            },
+            target: {
+                image: path.resolve(__dirname, 'sprite.png'),
+                css: path.resolve(__dirname, 'sprite.styl')
+            },
+            apiOptions: {
+                cssImageRef: "sprite.png"
+            }
+        })
 
     ],
 
