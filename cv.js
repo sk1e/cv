4ffda43c4fa088f6d36e6db4b8592bbf8c26ad82
Sergey Petrov <sekk1e@yandex.ru>
2016-11-01 13:24:35 +0700

    + profile, contact section

--- cv.js
+++ cv.js
@@ -1,9 +1,13 @@
 'use strict';
 
 require('./index.pug');
+
 require('./fonts.styl');
 require('./blocks/photo.styl');
 require('./blocks/author.styl');
 require('./blocks/cv.styl');
 require('./blocks/title.styl');
+require('./blocks/section.styl');
+require('./blocks/contact-table.styl');
 require('./icons.styl');
+require('./global.styl');
